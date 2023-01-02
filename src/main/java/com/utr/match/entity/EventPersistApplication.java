ef7a9712b61154d8545282fc91a13c503bd88be0
austinxyz <Austin.xyz@gmail.com>
2023-01-02 12:34:54 -0800

    persist event and division inforamtion into database

--- src/main/java/com/utr/match/entity/EventPersistApplication.java
+++ src/main/java/com/utr/match/entity/EventPersistApplication.java
@@ -3,6 +3,7 @@ package com.utr.match.entity;
 import com.utr.match.TeamLoader;
 import com.utr.model.Division;
 import com.utr.model.Event;
+import com.utr.model.Player;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,22 +17,44 @@ public class EventPersistApplication {
 
     @Bean
     public CommandLineRunner mappingDemo(EventRepository eventRepository,
-                                         DivisionRepository divisionRepository) {
+                                         DivisionRepository divisionRepository,
+                                         PlayerRepository playerRepository) {
         return args -> {
 
-            Event event = TeamLoader.getInstance().getEvent(TeamLoader.DEFAULT_EVENT_ID);
-
-            EventEntity eventEntity = new EventEntity(event.getName(), "UTR", event.getId());
-
-            eventRepository.save(eventEntity);
-
-            eventEntity = eventRepository.findAll().iterator().next();
-
-            for (Division div : event.getDivisions()) {
-                DivisionEntity divEntity = new DivisionEntity(div.getName(), eventEntity);
-                divisionRepository.save(divEntity);
+            for (PlayerEntity player: playerRepository.findByFirstNameLike("%Ya%")) {
+                System.out.println(player.getLastName() + " " + player.getFirstName());
             }
 
         };
+    }
+
+    private void createNewPlayer(Player player, PlayerRepository playerRepository) {
+        PlayerEntity playerEntity = new PlayerEntity();
+        playerEntity.setGender(player.getGender());
+        playerEntity.setUtrId(player.getId());
+
+        String firstName = getFirstName(player.getName());
+        String lastName = getLastName(player.getName());
+        System.out.println(firstName);
+        System.out.println(lastName);
+        playerEntity.setFirstName(firstName);
+        playerEntity.setLastName(lastName);
+        playerRepository.save(playerEntity);
+    }
+
+    private String getFirstName(String name) {
+        int index = name.indexOf(" ");
+        while(index < name.length() && name.charAt(index+1)==' ') {
+            index++;
+        }
+        return name.substring(index+1);
+    }
+
+    private String getLastName(String name) {
+        int index = name.indexOf(" ");
+        while(index < name.length() && name.charAt(index+1)==' ') {
+            index++;
+        }
+        return name.substring(0, index);
     }
 }
