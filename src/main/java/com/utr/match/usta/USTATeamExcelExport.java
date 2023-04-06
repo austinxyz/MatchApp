12e59762cd18291c829c94bd80f1f221b8e9bc48
austinxyz <Austin.xyz@gmail.com>
2023-04-05 20:08:13 -0700

    refine excel export function

--- src/main/java/com/utr/match/usta/USTATeamExcelExport.java
+++ src/main/java/com/utr/match/usta/USTATeamExcelExport.java
@@ -3,6 +3,7 @@ package com.utr.match.usta;
 import com.utr.match.entity.USTAMatch;
 import com.utr.match.entity.USTAMatchLine;
 import com.utr.match.entity.USTATeamMember;
+import org.apache.poi.common.usermodel.HyperlinkType;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.*;
 import org.springframework.web.servlet.view.document.AbstractXlsxView;
@@ -27,15 +28,79 @@ public class USTATeamExcelExport extends AbstractXlsxView {
         @SuppressWarnings("unchecked")
         NewUSTATeam team = (NewUSTATeam) model.get("team");
 
+        CreationHelper helper = workbook.getCreationHelper();
+
         // create one sheet
         Sheet sheet = workbook.createSheet("Team" );
 
-        int rowNum = createTeamPlayerInfo(team, sheet);
+        CellStyle hylinkStyle = workbook.createCellStyle();
+        Font hylinkFont = workbook.createFont();
+        hylinkFont.setUnderline(Font.U_SINGLE);
+        hylinkFont.setColor(IndexedColors.BLUE.index);
+        hylinkStyle.setFont(hylinkFont);
+
+        int rowNum = createTeamPlayerInfo(team, sheet, helper, hylinkStyle);
 
         rowNum = createPlayOffMatchInfo(team, sheet, rowNum + 1);
 
         rowNum = createTeamSummary(team, sheet, rowNum + 1);
 
+        rowNum = createLineStatInfo(team, sheet, rowNum + 1);
+
+    }
+
+    private int createLineStatInfo(NewUSTATeam team, Sheet sheet, int startRow) {
+        int rowNum = startRow;
+
+        for (USTADoubleLineStat doubleStat: team.getDoubleLineStats().values()) {
+
+            Row teamRow = sheet.createRow(rowNum++);
+            teamRow.createCell(0).setCellValue("Line:" + doubleStat.getLineName());
+            teamRow.createCell(1).setCellValue("W:" + doubleStat.getWinMatchNo() +"/ L:" + doubleStat.getLostMatchNo()
+                + " - " + String.format("%.2f", doubleStat.getWinPrecent()*100) + "%");
+
+            teamRow = sheet.createRow(rowNum++);
+            teamRow.createCell(0).setCellValue("#");
+            teamRow.createCell(1).setCellValue("Pair");
+            teamRow.createCell(2).setCellValue("W/L");
+
+            int index=1;
+            for (NewUSTATeamPair pair:doubleStat.getPairs()) {
+                teamRow = sheet.createRow(rowNum++);
+                teamRow.createCell(0).setCellValue(index++);
+                if (pair.getPlayer1()!=null) {
+                    teamRow.createCell(1).setCellValue(pair.getPlayer1().getPlayerInfo(false));
+                }
+                teamRow.createCell(2).setCellValue(pair.getWinMatchNo() + " / " + pair.getLostMatchNo());
+                teamRow = sheet.createRow(rowNum++);
+                if (pair.getPlayer2()!=null) {
+                    teamRow.createCell(1).setCellValue(pair.getPlayer2().getPlayerInfo(false));
+                }
+            }
+        }
+
+        if (team.getSingleLineStats() != null) {
+            for (USTASingleLineStat singleStat : team.getSingleLineStats().values()) {
+
+                Row teamRow = sheet.createRow(rowNum++);
+                teamRow.createCell(0).setCellValue("Line:" + singleStat.getLineName());
+                teamRow.createCell(1).setCellValue("W:" + singleStat.getWinMatchNo() + "/ L:" + singleStat.getLostMatchNo()
+                        + " - " + String.format("%.2f", singleStat.getWinPrecent() * 100));
+
+                teamRow = sheet.createRow(rowNum++);
+                teamRow.createCell(0).setCellValue("Pair");
+                teamRow.createCell(1).setCellValue("W/L");
+
+                for (NewUSTATeamSingle single : singleStat.getSinglers()) {
+                    teamRow = sheet.createRow(rowNum++);
+                    if (single.getPlayer() != null) {
+                        teamRow.createCell(0).setCellValue(single.getPlayer().getPlayerInfo(true));
+                    }
+                    teamRow.createCell(1).setCellValue(single.getWinMatchNo() + " / " + single.getLostMatchNo());
+                }
+            }
+        }
+        return rowNum;
     }
 
     private int createTeamSummary(NewUSTATeam team, Sheet sheet, int startRow) {
@@ -48,9 +113,23 @@ public class USTATeamExcelExport extends AbstractXlsxView {
 
         teamRow = sheet.createRow(rowNum++);
         teamRow.createCell(0).setCellValue("Best Double by UTR");
-        teamRow.createCell(1).setCellValue(team.getBestUTRDouble().getPairInfo());
+        NewUSTATeamPair bestUTRDouble = team.getBestUTRDouble();
+        if (bestUTRDouble.getPlayer1()!=null) {
+            teamRow.createCell(1).setCellValue(bestUTRDouble.getPlayer1().getPlayerInfo(false));
+        }
         teamRow.createCell(2).setCellValue("Best Double by DR");
-        teamRow.createCell(3).setCellValue(team.getBestDRDouble().getPairInfo());
+        NewUSTATeamPair bestDRDouble = team.getBestDRDouble();
+        if (bestDRDouble.getPlayer1() !=null) {
+            teamRow.createCell(3).setCellValue(bestDRDouble.getPlayer1().getPlayerInfo(false));
+        }
+
+        teamRow = sheet.createRow(rowNum++);
+        if (bestUTRDouble.getPlayer2()!=null) {
+            teamRow.createCell(1).setCellValue(bestUTRDouble.getPlayer2().getPlayerInfo(false));
+        }
+        if (bestDRDouble.getPlayer2() !=null) {
+            teamRow.createCell(3).setCellValue(bestDRDouble.getPlayer2().getPlayerInfo(false));
+        }
 
         if (team.getBestUTRSingle()!=null) {
             teamRow = sheet.createRow(rowNum++);
@@ -69,9 +148,17 @@ public class USTATeamExcelExport extends AbstractXlsxView {
         for (USTADoubleLineStat doubleStat: team.getDoubleLineStats().values()) {
             teamRow = sheet.createRow(rowNum++);
             teamRow.createCell(0).setCellValue(doubleStat.getLineName());
-            teamRow.createCell(1).setCellValue(doubleStat.bestPair().getPairInfo());
+            NewUSTATeamPair bestPair = doubleStat.bestPair();
+            if (bestPair.getPlayer1()!=null) {
+                teamRow.createCell(1).setCellValue(bestPair.getPlayer1().getPlayerInfo(false));
+            }
             teamRow.createCell(2).setCellValue(doubleStat.getWinMatchNo() + "/" + doubleStat.getLostMatchNo() + "(" + String.format("%.2f", doubleStat.getWinPrecent()*100) + "%)");
             teamRow.createCell(3).setCellValue(String.format("%.2f",doubleStat.averageUTRs()));
+
+            teamRow = sheet.createRow(rowNum++);
+            if (bestPair.getPlayer2()!=null) {
+                teamRow.createCell(1).setCellValue(bestPair.getPlayer2().getPlayerInfo(false));
+            }
         }
 
         if (team.getBestUTRSingle()!=null) {
@@ -106,13 +193,30 @@ public class USTATeamExcelExport extends AbstractXlsxView {
                 matchRow.createCell(4).setCellValue("Win Team");
 
                 for (USTAMatchLine line : lines) {
-                    Row row = sheet.createRow(rowNum++);
+
                     boolean isSingle = line.getType().equals("S");
+                    Row row = sheet.createRow(rowNum++);
                     row.createCell(0).setCellValue(line.getName());
-                    row.createCell(1).setCellValue(line.getHomePair().getPairInfo(isSingle));
-                    row.createCell(2).setCellValue(line.getGuestPair().getPairInfo(isSingle));
+                    NewUSTATeamPair homePair = line.getHomePair();
+                    if (homePair.getPlayer1()!=null) {
+                        row.createCell(1).setCellValue(homePair.getPlayer1().getPlayerInfo(isSingle));
+                    }
+                    NewUSTATeamPair guestPair = line.getGuestPair();
+                    if (guestPair.getPlayer1()!=null) {
+                        row.createCell(2).setCellValue(guestPair.getPlayer1().getPlayerInfo(isSingle));
+                    }
                     row.createCell(3).setCellValue(line.getScore());
                     row.createCell(4).setCellValue(line.isHomeTeamWin()? "Home":"Away");
+
+                    if (!isSingle) {
+                        row = sheet.createRow(rowNum++);
+                        if (homePair.getPlayer2()!=null) {
+                            row.createCell(1).setCellValue(homePair.getPlayer2().getPlayerInfo(isSingle));
+                        }
+                        if (guestPair.getPlayer2()!=null) {
+                            row.createCell(2).setCellValue(guestPair.getPlayer2().getPlayerInfo(isSingle));
+                        }
+                    }
                 }
             }
         }
@@ -120,7 +224,7 @@ public class USTATeamExcelExport extends AbstractXlsxView {
         return rowNum;
     }
 
-    private int createTeamPlayerInfo(NewUSTATeam team, Sheet sheet) {
+    private int createTeamPlayerInfo(NewUSTATeam team, Sheet sheet, CreationHelper helper, CellStyle hylinkSytle) {
 
         int rowNum = 0;
 
@@ -129,13 +233,26 @@ public class USTATeamExcelExport extends AbstractXlsxView {
         teamRow.createCell(1).setCellValue(team.getTeamName());
         teamRow.createCell(2).setCellValue("Area");
         teamRow.createCell(3).setCellValue(team.getArea());
+        teamRow.createCell(4).setCellValue("");
+
         teamRow = sheet.createRow(rowNum++);
         teamRow.createCell(0).setCellValue("Captain");
         teamRow.createCell(1).setCellValue(team.getCaptainName());
-        teamRow.createCell(2).setCellValue("USTA Link");
-        teamRow.createCell(3).setCellValue(team.getLink());
-        teamRow.createCell(4).setCellValue("Tennis Record Link");
-        teamRow.createCell(5).setCellValue(team.getTennisRecordLink());
+        Cell ustaLink = teamRow.createCell(2);
+        ustaLink.setCellValue("USTA Link");
+        Hyperlink link = helper.createHyperlink(HyperlinkType.URL);
+        link.setAddress(team.getLink());
+        ustaLink.setHyperlink(link);
+        ustaLink.setCellStyle(hylinkSytle);
+
+        Cell trLink = teamRow.createCell(3);
+        trLink.setCellValue("Tennis Record Link");
+        link = helper.createHyperlink(HyperlinkType.URL);
+        String trLinkURL = team.getTennisRecordLink().replace(" ", "%20");
+        link.setAddress(trLinkURL);
+        trLink.setHyperlink(link);
+        trLink.setCellStyle(hylinkSytle);
+        teamRow.createCell(4).setCellValue("");
 
         // create row0 as a header
         Row row0 = sheet.createRow(rowNum++);
@@ -149,13 +266,32 @@ public class USTATeamExcelExport extends AbstractXlsxView {
         int index = 1;
         // create row1 onwards from List<T>
         for(USTATeamMember member: team.players) {
+
+            if (!member.isQualifiedPo()) {
+                continue;
+            }
             Row row = sheet.createRow(rowNum++);
             row.createCell(0).setCellValue(index++);
-            row.createCell(1).setCellValue(member.getName() + "(" + member.getGender() + ") - " + member.getUSTARating());
+            Cell player = row.createCell(1);
+            player.setCellValue(member.getName() + "(" + member.getGender() + ") - " + member.getUSTARating());
+            link = helper.createHyperlink(HyperlinkType.URL);
+            link.setAddress(member.getNoncalLink());
+            player.setHyperlink(link);
+            player.setCellStyle(hylinkSytle);
+
             row.createCell(2).setCellValue(member.getDynamicRating());
-            row.createCell(3).setCellValue(member.getWinNo() + "/" + member.getLostNo());
-            row.createCell(4).setCellValue(member.getDUTR() + "D(" + member.getDUTRStatus() + ")/" + member.getSUTR()+"S("+member.getSUTRStatus()+")");
+            row.createCell(3).setCellValue(member.getWinNo() + "/" + member.getLostNo() + "(" + String.format("%.2f", member.getWinPercent()*100) + "%)");
+            Cell utr = row.createCell(4);
+            utr.setCellValue(member.getDUTR() + "D(" + member.getDUTRStatus() + ")/" + member.getSUTR()+"S("+member.getSUTRStatus()+")");
+            if (member.getUtrId()!=null && !member.getUtrId().trim().equals("")) {
+                link = helper.createHyperlink(HyperlinkType.URL);
+                link.setAddress("https://app.universaltennis.com/profiles/" + member.getUtrId());
+                utr.setHyperlink(link);
+                utr.setCellStyle(hylinkSytle);
+            }
+
             row.createCell(5).setCellValue(String.format("%.2f", member.getSuccessRate()*100) + "%/" + String.format("%.2f",member.getWholeSuccessRate()*100)+"%");
+            row.createCell(6).setCellValue("");
         }
 
         return rowNum;
